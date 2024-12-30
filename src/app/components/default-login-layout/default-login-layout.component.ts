ee2c5d5e1329851aed2d6712670f84587180afbd
samantabarros <samantadesenvolvedora@gmail.com>
2024-12-30 19:01:23 -0300

    feat: adicionado estilo a layout padrão

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
   imports: [],
   templateUrl: './default-login-layout.component.html',
-  styleUrl: './default-login-layout.component.css'
+  styleUrl: './default-login-layout.component.scss'
 })
 export class DefaultLoginLayoutComponent {
-
+  @Input() title: string = "";
+  @Input() primaryBtn: string = "";
+  @Input() secondaryBtn: string="";
 }
